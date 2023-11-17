be02cd31de35d3918324dc251fafe117d8bbea47
Thiago Gonzalez <thiagogonzalez.dev@gmail.com>
2023-11-16 21:10:47 -0300

    first commit

---
+++ src/main/java/com/thiagogonzalez/filahovet/FilaHovetApplication.java
@@ -0,0 +1,13 @@
+package com.thiagogonzalez.filahovet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilaHovetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilaHovetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thiagogonzalez/filahovet/FilaHovetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagogonzalez.filahovet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilaHovetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
