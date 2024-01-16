02de6817145195548ebf4827262b124cf82ed670
Thiago Gonzalez <thiagogonzalez.dev@gmail.com>
2024-01-15 21:47:12 -0300

    configuration: configure scan and jpa repositories

--- src/main/java/com/thiagogonzalez/filahovet/FilaHovetApplication.java
+++ src/main/java/com/thiagogonzalez/filahovet/FilaHovetApplication.java
@@ -2,8 +2,12 @@ package com.thiagogonzalez.filahovet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("com.thiagogonzalez.filahovet.domain")
+@EnableJpaRepositories("com.thiagogonzalez.filahovet.repositories")
 public class FilaHovetApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/thiagogonzalez/filahovet/config/AppConfig.java
@@ -0,0 +1,9 @@
+package com.thiagogonzalez.filahovet.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.thiagogonzalez.filahovet.services")
+public class AppConfig {
+}
